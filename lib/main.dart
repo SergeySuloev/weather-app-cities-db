a5a909495f04515b03025b01353d14e36adaeda3
Sergey <super.suloev@gmail.com>
2024-06-12 15:41:35 +0300

    Created a base for city class

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import "package:hive/hive.dart";
+import "package:path_provider/path_provider.dart";
 
+class OpenWeatherMapCity {
+  int id;
+  String name;
+  String state;
+  String countryCode;
+  double coordLon;
+  double coordLat;
+
+  OpenWeatherMapCity(this.id, this.name, this.state, this.countryCode,
+      this.coordLon, this.coordLat);
+}
+
+void main() async {}
