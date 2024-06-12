b2b0b7653a0406284e1c4c1018d8a104aaa28e66
Sergey <super.suloev@gmail.com>
2024-06-12 15:47:54 +0300

    Initialized Hive DB for usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import "package:hive/hive.dart";
 import "package:path_provider/path_provider.dart";
 
+part 'main.g.dart';
+
+@HiveType(typeId: 0)
 class OpenWeatherMapCity {
+  @HiveField(0)
   int id;
+
+  @HiveField(1)
   String name;
+
+  @HiveField(2)
   String state;
+
+  @HiveField(3)
   String countryCode;
+
+  @HiveField(4)
   double coordLon;
+
+  @HiveField(5)
   double coordLat;
 
   OpenWeatherMapCity(this.id, this.name, this.state, this.countryCode,
